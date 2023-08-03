9a8fa7da282f9e2d05d8cc721c06685bf33c497d
Juan David Peña <juandavidp76@hotmail.com>
2023-08-03 15:39:02 -0500

    fix vercel reload

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import store from './store'
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js'
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 const stripePromise = loadStripe(
   import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
     <Elements stripe={stripePromise}>
+      <RouterProvider router={router} />
       <App />
     </Elements>
     </Provider>
